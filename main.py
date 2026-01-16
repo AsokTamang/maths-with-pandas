7650064c91be742ee2f744319d506b904d02a7d5
AsokTamang <ashoktmg2058@gmail.com>
2026-01-16 12:16:31 -0500

    sorting the sold quantity column above 90th percentile value

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ import pandas as pd
 df = pd.read_csv("C:/Users/ashok/Downloads/chapter4_assets/chapter4_assets/4_shoe_sale_analysis/shoe_sales.csv")
 
 print(df[df.sold_qty<12.25].shape)
+
+df_new=df[df['sold_qty']>19.75].sort_values('sold_qty')
+print(df_new)
