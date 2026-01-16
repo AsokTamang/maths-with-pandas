b4f5d60b668ce635d9812afd3f6aabe5c7d576d2
AsokTamang <ashoktmg2058@gmail.com>
2026-01-16 12:31:03 -0500

    finding the null values

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ print(df[df.sold_qty<12.25].shape)
 df_new=df[df['sold_qty']>19.75].sort_values('sold_qty')
 print(df_new)
 print(df_new['sold_qty'].quantile(0.5))  #finding the 50th percentile value among the sold_qty dataset having higher value or equal to the 90th percentile value among total sold qty dataset
+
+
+###NIKE ANALYSIS
+df_nike=df[df['brand']=='Nike']
+df_nike.fillna(0,inplace=True)
+print(df_nike)
+fiftyth_value=df_nike['sold_qty'].quantile(0.5).astype(int)
+print(fiftyth_value)
+
+df_nike2=df[df['brand']=='Nike']
+df_nike2.isnull()
+
+
+
