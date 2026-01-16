965758ed85fb8bfaf361a62359bcb261d5353b26
AsokTamang <ashoktmg2058@gmail.com>
2026-01-16 13:42:20 -0500

    converting the types of data into same

--- prac.py
+++ prac.py
@@ -1,5 +1,24 @@
 #import necessary libraries
 import pandas as pd
 df = pd.read_csv("C:/Users/ashok/Downloads/chapter4_exercise1/chapter4_exercise1/telecom_customer_churn.csv")
-df.shape
+print(df.shape)
 df.head(5)
+
+#checking the datas
+print(df.columns)
+print(df.isnull())
+print(df.info())   #checking the types and whether the column data is null or not
+print(df.describe())
+
+#converting the numeric column data types into same int type
+#display all the column names
+df['total_charges'] = pd.to_numeric(df.tenure)
+df['monthly_charges'] = pd.to_numeric(df.tenure)
+df['tenure'] = pd.to_numeric(df.tenure)
+print(df.info())
+
+
+
+
+
+
