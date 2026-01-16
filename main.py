ba5b28b4c26ea489484e8c02730d4ad453480145
AsokTamang <ashoktmg2058@gmail.com>
2026-01-16 12:12:09 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#so for viewing data based on the specific threshold ,
+#we must use the horizontal barchart if the number of categories are many or we can also use the vertical barchart
+
+#but for viewing the continuous datas, we must use the line chart or line graph.
+
+import pandas as pd
+df = pd.read_csv("C:/Users/ashok/Downloads/chapter4_assets/chapter4_assets/4_shoe_sale_analysis/shoe_sales.csv")
