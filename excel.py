83f80fdd65b291119e1a5ecfa76022c8fafebee1
AsokTamang <ashoktmg2058@gmail.com>
2026-01-17 01:10:57 -0500

    finding the total sales outputing in an array based on the unique regions in a dataset

--- excel.py
+++ excel.py
@@ -2,21 +2,34 @@ import pandas as pd
 import openpyxl
 
 df_xl = pd.read_excel("C:/Users/ashok/Downloads/chapter4_assets/chapter4_assets/7_outlier_detection_using_iqr_boxplot/region_wise_sales.xlsx")
+
+region_names = df_xl.Region.unique()
+total_value = [df_xl[df_xl['Region'] == region].Sales for region in region_names]
+
+
 df_apac = df_xl[df_xl['Region'] == 'APAC']
 df_euro = df_xl[df_xl['Region'] == 'Europe']
 df_america = df_xl[df_xl['Region'] == 'Americas']
-qap1,qap2,qap3 = df_apac.Sales.quantile([0.25,0.50,0.75])
-IQRap= qap3-qap1
-lower_limit = qap1 - (1.5 * IQRap)
-upper_limit = qap3 + (1.5 * IQRap)
+
+
+def get_third_first_quartile(data):
+    q1,q3 = data.Sales.quantile([0.25,0.75])
+    iqr = q3 - q1
+    lower_limit = q1 - 1.5 * iqr
+    upper_limit = q3 + 1.5 * iqr
+    return lower_limit, upper_limit
+
+
+
+lower_limit,upper_limit = get_third_first_quartile(df_apac)
+print(lower_limit,upper_limit)
 outliersap = df_apac[(df_apac['Sales']<lower_limit) | (df_apac['Sales']>upper_limit)]
 print(outliersap)
 
 
-qe1,qe2,qe3 = df_euro.Sales.quantile([0.25,0.50,0.75])
-IQRe= qe3-qe1
-lower_limit_e = qe1 - (1.5 * IQRe)
-upper_limit_e = qe3 + (1.5 * IQRe)
+
+lower_limit_e , upper_limit_e = get_third_first_quartile(df_euro)
 outlierse = df_euro[(df_euro['Sales']<lower_limit_e) | (df_euro['Sales']>upper_limit_e)]
 print(outlierse)
+
 
