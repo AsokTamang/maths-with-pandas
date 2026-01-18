d392019f9f4f814897c15a9951bb655f4c3bbd2a
AsokTamang <ashoktmg2058@gmail.com>
2026-01-17 20:15:40 -0500

    calculating the range and iqr for sleep_duration column

--- fitness_tracker.py
+++ fitness_tracker.py
@@ -12,3 +12,15 @@ df.head(5)
 print(df['steps_taken'].mean())  #mean of steps taken
 print(df['calories_burned'].mean())  #mean of calories burned
 print(df.describe())   #getting the statistical datas on every numerical columns
+
+
+range_steps_taken = df.steps_taken.max() - df.steps_taken.min()
+print(range_steps_taken)
+# Calculate the range of "calories_burned" for the entire week
+range_calories_burned = df.calories_burned.max() - df.calories_burned.min()
+print(range_calories_burned)
+
+# Calculate the Interquartile Range (IQR) for "sleep_duration(hours)"
+qsd1,qsd2,qsd3 = df['sleep_duration(hours)'].quantile([0.25,0.5,0.75])
+iqrsd = qsd3 - qsd1
+print(iqrsd)
