db6de4b739095f7cf211adbbf367a28c471a4e33
AsokTamang <ashoktmg2058@gmail.com>
2026-01-19 12:26:26 -0500

    removing the outliers

--- removing_outliers.py
+++ removing_outliers.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-df = pd.read_csv("C:/Users/ashok/Downloads/chapter6_assets/chapter6_assets/3_normal_distribution/heights.csv")
+df = pd.read_csv("C:/Users/ashok/Downloads/chapter6_assets/chapter6_assets/4_detect_outliers_using_normal_distribution/heights_with_outliers.csv")
 sns.histplot(df,kde=True)
 mean =round(df.height.mean(),2)
 st_dev=round(df.height.std(),2)
@@ -17,6 +17,9 @@ plt.axvline(mean-(2*st_dev),color='blue',linestyle='--',linewidth =1)#one standa
 #three standard deviation
 plt.axvline(mean+(3*st_dev),color='red',linestyle='--',linewidth =1,label='±3 Std Dev')
 plt.axvline(mean-(3*st_dev),color='red',linestyle='--',linewidth =1)
+#the datas beyond the three standard deviation are the outliers
+plt.legend()
+plt.show()  #before removing the outliers
 
 left1 = mean-st_dev
 right1=mean+st_dev
@@ -30,7 +33,12 @@ right3=mean+(3*st_dev)
 df_within_onerange = df[(df.height>left1) & (df.height<right1)].sort_values(by='height')  #retrieving the column of heights within the first standard deviation and sorting them by heights
 print(df_within_onerange)
 
-plt.legend()
-plt.show()
+df_within_secondrange = df[(df.height>left2) & (df.height<right2)].sort_values(by='height')  #retrieving the column of heights within the first standard deviation and sorting them by heights
+print(df_within_secondrange)
 
+outliers = df[(df.height<left3) | (df.height>right3)]
+print(outliers)
 
+df= df[(df.height>left3) & (df.height<right3)]
+sns.histplot(df)  #removing the outliers
+plt.show()  #after removing the outliers
