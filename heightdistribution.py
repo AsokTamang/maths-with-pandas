2f7a2d4091b87131c9cb3d55d5248f19e04e16d6
AsokTamang <ashoktmg2058@gmail.com>
2026-01-19 11:57:01 -0500

    values within the first standard deviation range

--- heightdistribution.py
+++ heightdistribution.py
@@ -17,5 +17,18 @@ plt.axvline(mean-(2*st_dev),color='blue',linestyle='--',linewidth =1)#one standa
 #three standard deviation
 plt.axvline(mean+(3*st_dev),color='red',linestyle='--',linewidth =1,label='±3 Std Dev')
 plt.axvline(mean-(3*st_dev),color='red',linestyle='--',linewidth =1)
+
+left1 = mean-st_dev
+right1=mean+st_dev
+
+left2=mean-(2*st_dev)
+right2=mean+(2*st_dev)
+
+left3=mean-(3*st_dev)
+right3=mean+(3*st_dev)
+
+df_within_onerange = df[(df.height>left1) & (df.height<right1)].sort_values(by='height')  #retrieving the column of heights within the first standard deviation and sorting them by heights
+print(df_within_onerange)
+
 plt.legend()
 plt.show()
